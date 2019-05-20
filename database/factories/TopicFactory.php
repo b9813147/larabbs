137f85032952a45173f0e1e45423a0db7aad1d52
steven <b9813147@gmail.com>
2019-05-20 16:09:17 +0800

    + 填充假資料

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,16 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    // 隨機取得一個月時間
+    $updated_at = $faker->dateTimeThisMonth();
+    //傳參數生成最大時間不超過，因為創建時間需永遠比更改時間早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
     return [
-        // 'name' => $faker->name,
+        'title'      => $sentence,
+        'body'       => $faker->text(),
+        'excerpt'    => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
